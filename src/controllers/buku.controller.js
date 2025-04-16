bf78640917016998a0df3f3ac60907294eb603c8
Habiboys <nouvalhabibie18@gmail.com>
2025-04-16 12:32:49 +0700

    update

--- src/controllers/buku.controller.js
+++ src/controllers/buku.controller.js
@@ -49,12 +49,42 @@ const createBuku = (req, res) => {
 }
 
 const updateBuku = (req, res) => {
-  
-    
+  const id = parseInt(req.params.id);
+  const { judul, penulis, tahunTerbit } = req.body;
+  const bukuIndex = buku.findIndex((b) => b.id === id);
+
+  if (bukuIndex !== -1) {
+    buku[bukuIndex] = {
+      ...buku[bukuIndex],
+      judul,
+      penulis,
+      tahunTerbit
+    };
+    res.json({
+      status: 200,
+      message: 'Buku berhasil diperbarui',
+      data: buku[bukuIndex]
+    });
+  } else {
+    res.status(404).json({ message: 'Buku tidak ditemukan' });
+  }
 }
 
 const deleteBuku = (req, res) => {
-    
+  const id = parseInt(req.params.id);
+  const bukuIndex = buku.findIndex((b) => b.id === id);
+  
+  if (bukuIndex !== -1) {
+    const deletedBook = buku[bukuIndex];
+    buku.splice(bukuIndex, 1);
+    res.json({
+      status: 200,
+      message: 'Buku berhasil dihapus',
+      data: deletedBook
+    });
+  } else {
+    res.status(404).json({ message: 'Buku tidak ditemukan' });
+  }
 }
 
 module.exports = {
